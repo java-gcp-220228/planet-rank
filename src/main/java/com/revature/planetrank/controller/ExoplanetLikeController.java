a4affd53fb9ff9281e929914876e9c20b28695e4
Ankur Shahi <shahiankur80@gmail.com>
2022-04-14 18:56:17 -0400

    Exoplanet Like controller added

---
+++ src/main/java/com/revature/planetrank/controller/ExoplanetLikeController.java
@@ -0,0 +1,21 @@
+package com.revature.planetrank.controller;
+
+import com.revature.planetrank.model.ExoplanetLikes;
+import com.revature.planetrank.service.ExoplanetLikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExoplanetLikeController {
+
+    @Autowired
+    private ExoplanetLikeService exoplanetLikeService;
+
+    @PostMapping("/like")
+    public ExoplanetLikes addlike(@RequestBody ExoplanetLikes like){
+        return exoplanetLikeService.createLike(like);
+    }
+}
