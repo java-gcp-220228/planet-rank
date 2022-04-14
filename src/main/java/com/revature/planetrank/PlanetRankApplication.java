85fe1f1f0a50c63a323c667b6d0f23ff44e3ae41
bryce-f <fransenbryce@gmail.com>
2022-04-14 13:32:14 -0700

    removed files, and added GalleryController, ImageRepository, and Image

--- src/main/java/com/revature/planetrank/PlanetRankApplication.java
+++ src/main/java/com/revature/planetrank/PlanetRankApplication.java
@@ -1,25 +1,20 @@
 package com.revature.planetrank;
 
-import com.revature.planetrank.model.ExoplanetComments;
-
-import com.revature.planetrank.model.Exoplanet;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class PlanetRankApplication {
 
 	public static void main(String[] args) {
-//		//Exoplanet exoplanet = new Exoplanet();
-		//User author = new User();
-
-		//ExoplanetComments comment = new ExoplanetComments("comment", author, null);
-		//ExoplanetLikes likes = new ExoplanetLikes();
-		//System.out.println(new Exoplanet("JaysPlanet", "AnkurSun", 2, 1, 2022, 100192293, 13124123, comment, likes));
 
 		SpringApplication.run(PlanetRankApplication.class, args);
 
 	}
-
 }
--- src/main/java/com/revature/planetrank/controller/UsersController.java
+++
@@ -1,4 +0,0 @@
-package com.revature.planetrank.controller;
-
-public class UsersController {
-}
