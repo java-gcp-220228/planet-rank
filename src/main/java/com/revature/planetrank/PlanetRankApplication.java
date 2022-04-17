b1b2a7495dd9574bd7b0a6bf6e305b1c003f5288
Ankur Shahi <shahiankur80@gmail.com>
2022-04-17 17:20:01 -0400

    logging Aspect added

--- src/main/java/com/revature/planetrank/PlanetRankApplication.java
+++ src/main/java/com/revature/planetrank/PlanetRankApplication.java
@@ -2,11 +2,13 @@ package com.revature.planetrank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableAspectJAutoProxy
 public class PlanetRankApplication {
 
 	public static void main(String[] args) {
