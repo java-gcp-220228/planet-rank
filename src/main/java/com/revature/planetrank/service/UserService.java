310c22a54c915ab0dd7f8cbc26ca608388251b48
Ankur Shahi <shahiankur80@gmail.com>
2022-04-18 02:18:11 -0400

    set the minimum limit of 8 characters for userid and password

--- src/main/java/com/revature/planetrank/service/UserService.java
+++ src/main/java/com/revature/planetrank/service/UserService.java
@@ -3,6 +3,7 @@ package com.revature.planetrank.service;
 import com.revature.planetrank.dao.UserRepository;
 import com.revature.planetrank.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,13 @@ public class UserService {
 
     public final UserRepository userRepository;
 
-    public User createUser(User user){
+    public User createUser(User user) throws IllegalArgumentException{
+
+        if(user.getUsername().length()<=8 || user.getPassword().length()<=8){
+            throw new IllegalArgumentException("username and password must be 8 characters long");
+        }
+        // Encrypt password
+        user.setPassword(BCrypt.hashpw(user.getPassword(),BCrypt.gensalt()));
         User newUser = userRepository.save(user);
         return newUser;
     };
